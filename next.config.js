0cfdd28ca2eda6d3d17bcfad269b8d0da965631d
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-02-28 18:54:45 +0600

    Update next.config.js with new headers and options

--- next.config.js
+++ next.config.js
@@ -1,18 +1,29 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
 
-  images: {
-    domains: [],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
   },
-
-  experimental: {
-    optimizeCss: true,
-  },
-
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  }
 };
 
 module.exports = nextConfig;
